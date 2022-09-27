cab9781c51c349e81d7cdb33a16f3e228b5590a5
EmP33 <purwinmarek16@gmail.com>
2022-09-27 18:26:06 +0200

    Fixed gatsby-plugin-manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `borgaz-website`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
@@ -28,7 +28,6 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
