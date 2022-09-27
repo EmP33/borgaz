f8fb725be9fc91d65b59f81985974f02ef42035b
EmP33 <purwinmarek16@gmail.com>
2022-09-27 18:35:06 +0200

    Delete gatsby-plugin-manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -17,19 +17,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `borgaz-website`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-      },
-    },
   ],
   jsxRuntime: `automatic`,
 };
